50be3db88ae03d149b3c724075624cb7e4ca207e
rioseleng09 <94964007+rioseleng09@users.noreply.github.com>
2023-12-13 00:08:44 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+import pickle
+
+# Function to load the model
+def load_model():
+    # Load model architecture from JSON file
+    with open('model.json', 'r') as json_file:
+        loaded_model_json = json_file.read()
+
+    # Close the JSON file
+    model = model_from_json(loaded_model_json)
+
+    # Load weights into the new model
+    model.load_weights("model.h5")
+
+    return model
+
+# Function to make a diagnosis
+def diagnosis(file, model):
+    # Load and preprocess the image
+    img = image.load_img(file, target_size=(IMM_SIZE, IMM_SIZE), color_mode="grayscale")
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0  # Normalize to [0, 1]
+
+    # Predict the diagnosis
+    predicted_probabilities = model.predict(img_array)
+    predicted_class = np.argmax(predicted_probabilities, axis=-1)[0]
+
+    # Map the predicted class to the diagnosis
+    diagnosis_mapping = {0: "Diagnosis_A", 1: "Diagnosis_B", 2: "Diagnosis_C"}
+    predicted_diagnosis = diagnosis_mapping[predicted_class]
+
+    return predicted_diagnosis
+
+# Main Streamlit app
+def main():
+    st.title("Medical Image Diagnosis App")
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+
+        # Load the model
+        model = load_model()
+
+        # Get diagnosis
+        result = diagnosis(uploaded_file, model)
+        
+        st.success(f"The predicted diagnosis is: {result}")
+
+if __name__ == "__main__":
+    main()
